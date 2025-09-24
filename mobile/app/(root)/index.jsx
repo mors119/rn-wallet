dbce76ec1911c676966d416bec4d20d2fa3d7d54
heesk0223@gmail.com <heesk0223@gmail.com>
2025-09-25 08:22:00 +0900

    05: cors, rn-data-hook

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -2,9 +2,22 @@ import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 import { SignOutButton } from '@/components/SignOutButton';
+import { useTransactions } from '../../hook/useTransactions';
+import { useEffect } from 'react';
 
 export default function Page() {
   const { user } = useUser();
+  const { transactions, summary, isLoading, loadData, deleteTransaction } =
+    useTransactions(user.id);
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  console.log(user.id);
+
+  console.log(transactions);
+  console.log(summary);
 
   return (
     <View>
